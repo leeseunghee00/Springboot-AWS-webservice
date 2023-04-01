99d6602d5d88d91e795c70ea82c6c701ec8b9896
leeseunghee00 <lsh9246@gmail.com>
2023-04-02 03:22:48 +0900

    feat: spring security 설정

---
+++ src/main/java/com/seunghee/springboot/domain/user/Role.java
@@ -0,0 +1,17 @@
+package com.seunghee.springboot.domain.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+    /**
+     * 스프링 싴리티에서는 권한 코드에 항상 ROLE_xx 로 지정한다.
+     */
+    GUEST("ROLE_GUEST", "손님"),
+    USER("ROLE_USER", "일반 사용자");
+
+    private final String key;
+    private final String title;
+}
