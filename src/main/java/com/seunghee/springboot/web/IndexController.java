b9af8a4c2a065a35d70ed43a2ddb046b44078174
leeseunghee00 <lsh9246@gmail.com>
2023-04-02 03:56:43 +0900

    feat: Google 로그인 연동

--- src/main/java/com/seunghee/springboot/web/IndexController.java
+++ src/main/java/com/seunghee/springboot/web/IndexController.java
@@ -1,22 +1,32 @@
 package com.seunghee.springboot.web;
 
+import com.seunghee.springboot.config.auth.dto.SessionUser;
 import com.seunghee.springboot.service.posts.PostsService;
 import com.seunghee.springboot.web.dto.PostsResponseDto;
-import com.sun.org.apache.xpath.internal.operations.Mod;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor    //final이 붙거나 @NotNull 이 붙은 필드의 생성자를 자동 생성해주는 롬복 어노테이션
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {  //Model = 서버 템플릿에서 사용할 수 잇는 객체를 저장할 수 있다.
         model.addAttribute("posts", postsService.findALlDsec());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";     //index.mustache로 반환되어 view resolver가 처리하게 된다.
     }
 
